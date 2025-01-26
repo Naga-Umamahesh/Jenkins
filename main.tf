4a8bc1e101fea4dbf22e6f899d22d554e7652f52
Mahesh <umamaheshbatraju@gmail.com>
2025-01-26 17:36:52 +0530

    Modified

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 resource "aws_instance" "main" {
   ami = "ami-0fa40e25bf4dda1f6"
   instance_type = "t2.micro"
   key_name = "Mahi"
 }
-=======
-
->>>>>>> e24c5021f7f6f9fd1db45fe1df7cf2da2f018cbb
