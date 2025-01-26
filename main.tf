72be0a96e60609e770f11ab18eb14fbc2703f7c0
Mahesh <umamaheshbatraju@gmail.com>
2025-01-26 17:48:05 +0530

    Modified

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-resource "aws_instance" "main" {
-  ami = "ami-0fa40e25bf4dda1f6"
-  instance_type = "t2.micro"
-  key_name = "Mahi"
+# resource "aws_instance" "main" {
+#   ami = "ami-0fa40e25bf4dda1f6"
+#   instance_type = "t2.micro"
+#   key_name = "Mahi"
+# }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket-5678"
 }
+
